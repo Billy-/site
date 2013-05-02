e4a2242588110082f2c90fd38f93c935357de71c
Billy- <billy@studiopretty.com>
2013-05-02 02:51:17 +0200

    Create head.php
    referenced css/styles.css, scripts/scripts.js and jQuery

---
+++ head.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="css/styles.css" type="text/css" >
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="scripts/scripts.js" type="text/javascript"></sciprt>
