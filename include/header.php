1001f2d3d11d4ec485ab8c81eb5f8aef5142e13c
Billy Mathews <billy@studiopretty.com>
2013-05-12 03:01:56 +0100

    Added footer & strapline

--- include/header.php
+++ include/header.php
@@ -1,4 +1,5 @@
 <div class="header">
 	<h1 id="site-title"><a href="<?php echo home_url(); ?>/">BeMaths</a></h1>
+	<h2>Billy Mathews: Website Developer and wannabe Games Developer.</h2>
 	<?php my_include('main_nav'); ?>
 </div>
