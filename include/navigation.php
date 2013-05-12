fe668a2138ad841424756b2088fb5045abc575f9
Billy Mathews <billy@studiopretty.com>
2013-05-12 03:06:14 +0100

    Remove Game from navigation

--- include/navigation.php
+++ include/navigation.php
@@ -2,6 +2,5 @@
 	<ul>
 		<li><a href="<?php echo home_url(); ?>/">Home</a></li>
 		<li><a href="<?php echo home_url(); ?>/about">About</a></li>
-		<li><a href="<?php echo home_url(); ?>/game">Game</a></li>
 	</ul>
 </div>
